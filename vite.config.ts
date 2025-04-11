3e1cbb5a9ddfc6fd59b79cc6e8211fd2214cfb71
Tyler Ballast <tyler.ballast@gmail.com>
2025-04-11 12:33:23 +0200

    Update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pos-bar/',  // Add this line with your repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
